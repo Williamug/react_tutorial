7e270b0d937b0670f11c3560961cc95f716d2961
williamdk <asabawilliamdk@yahoo.com>
2019-03-06 19:35:31 +0300

    add table component

---
+++ src/Table.js
@@ -0,0 +1,33 @@
+import React, {Component} from 'react';
+
+class Table extends Component{
+    render(){
+        return(
+            <table>
+                <thead>
+                    <tr>
+                        <th>Name</th>
+                        <th>Job</th>
+                    </tr>
+                </thead>
+
+                <tbody>
+                    <tr>
+                        <th>Mac</th>
+                        <th>Bouncer</th>
+                    </tr>
+                    <tr>
+                        <th>Dee</th>
+                        <th>Aspiring actress</th>
+                    </tr>
+                    <tr>
+                        <th>Dennis</th>
+                        <th>Bartender</th>
+                    </tr>
+                </tbody>
+            </table>
+        );
+    }
+}
+
+export default Table;
